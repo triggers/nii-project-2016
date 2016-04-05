e3d39d624509bd64224562179100152f3a04a2e8
Richard Potter <potter@axsh.net>
2016-04-05 18:19:28 +0900

    Bug fixes for "Reset Wakame-vdc" task.  Quick test works.

--- notebooks/stepdefs/s-introduction-terminate-wakame-instances/provided-script.sh
+++ notebooks/stepdefs/s-introduction-terminate-wakame-instances/provided-script.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-thescript="$(cat <<EEE
-reset-wakame.sh
-
-# This sleep is necessary so Wakame-vdc will reuse the 10.0.2.100 IP
-# addresss.
-sleep 15  # TODO: remove the need for this
-
-EEE
-)"
-
-echo "Now executing the following script to do this task:"
-echo "$thescript"
-echo
-echo "Output follows:"
-
-bash <<<"$thescript"
+bash
 
 echo "## End of Output ##"
