75e143c27eaf956b56a73f46fd80e49ff3b88f46
Richard Potter <potter@axsh.net>
2016-04-07 23:28:08 +0900

    Fix problem with bash stripping backslashes at end of lines

--- notebooks/stepdefs/s-imagebuild-insert-template/template.sh
+++ notebooks/stepdefs/s-imagebuild-insert-template/template.sh
@@ -10,10 +10,12 @@ reboot=false
 function load_config() {
     local file="/var/lib/jenkins/jobs/${job}/config.xml"
     local element_name="${1}" element_value="${2}"
-    ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
+    # The sed here preserves end of line backslashes from being stripped by the bash
+    # on the other side of ssh
+    sed 's,\\$,BaCkSlash,g'  <<EOF | ssh -i /home/centos/mykeypair root@10.0.2.100 2> /dev/null
         $(declare -f xml_load_backup)
 
-        value="\$(cat <<"XML_BLOCK"
+        value="\$(sed 's,BaCkSlash,\\\\,g' <<"XML_BLOCK"
 $element_value
 XML_BLOCK
 )"
