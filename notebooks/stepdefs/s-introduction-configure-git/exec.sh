e87052810efeb5e01d344d124275977fcf534965
toros11 <tobias.rosen@axsh.net>
2016-04-08 23:39:31 +0900

    Fixed old value from copy/paste

--- notebooks/stepdefs/s-introduction-configure-git/exec.sh
+++ notebooks/stepdefs/s-introduction-configure-git/exec.sh
@@ -1,6 +1,6 @@
 ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
     $(declare -f check_not_empty)
 
-    check_not_empty system_config "$filename" token && echo "Check [ ok ]" || echo "Check [ fail ]"
-    check_not_empty system_config "$filename" room && echo "Check [ ok ]" || echo "Check [ fail ]"
+    check_not_empty system_config "$filename" globalConfigName && echo "Check [ ok ]" || echo "Check [ fail ]"
+    check_not_empty system_config "$filename" golbalConfigEmail && echo "Check [ ok ]" || echo "Check [ fail ]"
 EOF
