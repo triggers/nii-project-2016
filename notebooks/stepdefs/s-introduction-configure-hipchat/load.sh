b6ce5811e3e2fa63936d3d0b8ae6aa2d9eb09dff
toros11 <tobias.rosen@axsh.net>
2016-04-08 23:37:50 +0900

    Don't link from base file

--- notebooks/stepdefs/s-introduction-configure-hipchat/load.sh
+++ notebooks/stepdefs/s-introduction-configure-hipchat/load.sh
@@ -1,1 +1,6 @@
-../common/load.sh
+#!/bin/bash
+
+. $(dirname $0)/stepdata.conf
+
+[[ -f $(dirname $0)/xml-data/"${filename##*/}" ]] &&
+    scp -i /home/centos/mykeypair $(dirname $0)/xml-data/"${filename##*/}"  root@10.0.2.100:"$filename"
--- notebooks/stepdefs/s-introduction-configure-hipchat/save.sh
+++ notebooks/stepdefs/s-introduction-configure-hipchat/save.sh
@@ -1,1 +1,5 @@
-../common/save.sh
+#!/bin/bash
+
+. $(dirname $0)/stepdata.conf
+
+scp -i /home/centos/mykeypair root@10.0.2.100:"$filename" $(dirname $0)/xml-data/ &> /dev/null
