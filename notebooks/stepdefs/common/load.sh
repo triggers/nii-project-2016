674d4fe25c21e1ed7398beab9f7591a6ceff81ec
toros11 <tobias.rosen@axsh.net>
2016-04-11 21:48:08 +0900

    Include insert as a possible parameter. move POST request to load files

--- notebooks/stepdefs/common/load.sh
+++ notebooks/stepdefs/common/load.sh
@@ -5,11 +5,9 @@
 . $(dirname $0)/stepdata.conf
 . /home/centos/notebooks/stepdefs/jenkins-utility/xml-utility.sh
 
-reboot=false
-
 function load_config() {
     local file="/var/lib/jenkins/jobs/${job}/config.xml"
-    local element_name="${1}" element_value="${2}"
+    local element_name="${1}" insert_type="${2}" element_value="${3}"
     ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF 2> /dev/null
         $(declare -f xml_load_backup)
 
@@ -17,16 +15,20 @@ function load_config() {
 $element_value
 XML_BLOCK
 )"
-        xml_load_backup "${file}" "${element_name}" "\${value}"
+        xml_load_backup "-${insert_type}" "${file}" "${element_name}" "\${value}"
+        curl -X POST http://localhost:8080/job/$job/config.xml --data-binary "@${file}"
 EOF
 }
 
 echo "Loading progress..."
 [[ ${#xpaths} -eq 0 ]] || {
     for xpath in "${xpaths[@]}" ; do
+	insert_type="replace"
+        [[ ${#insert_to} -eq 0 ]] || { contains_value "${xpath}" "${insert_to[@]}" && insert_type="insert" ; }
+
         student_file="$(dirname $0)/xml-data/${xpath##*/}.data-student"
         [[ -f ${student_file} ]] && {
-            load_config "${xpath##*/}" "$(cat ${student_file})"
+            load_config "${xpath##*/}" "${insert_type}" "$(cat ${student_file})"
         }
     done
 }
--- notebooks/stepdefs/common/provided-load.sh
+++ notebooks/stepdefs/common/provided-load.sh
@@ -5,10 +5,11 @@
 . /home/centos/notebooks/stepdefs/jenkins-utility/xml-utility.sh
 
 reboot=false
+insert_type="-replace"
 
 function load_config() {
     local file="/var/lib/jenkins/jobs/${job}/config.xml"
-    local element_name="${1}" element_value="${2}"
+    local element_name="${1}" insert_type="${2}"  element_value="${3}"
     ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
         $(declare -f xml_load_backup)
         $(declare -f xml_replace_case)
@@ -18,13 +19,18 @@ function load_config() {
 $element_value
 XML_BLOCK
 )"
-        xml_load_backup "${job}" "${file}" "${element_name}" "\${value}"
+        xml_load_backup "-${insert_type}" "${file}" "${element_name}" "\${value}"
+        curl -X POST http://localhost:8080/job/$job/config.xml --data-binary "@${file}"
+
 EOF
 }
 
 [[ ${#xpaths} -eq 0 ]] || {
     for xpath in "${xpaths[@]}" ; do
-	echo "Loading path ${xpath}."
-        load_config "${xpath##*/}" "$(cat $(dirname $0)/xml-data/${xpath##*/}.data)"
+        insert_type="replace"
+
+        [[ ${#insert_to} -eq 0 ]] || { contains_value "${xpath}" "${insert_to[@]}" && insert_type="insert" ; }
+        echo "Loading path ${xpath}. ($insert_type)"
+        load_config "${xpath##*/}" "${insert_type}" "$(cat $(dirname $0)/xml-data/${xpath##*/}.data)"
     done
 }
