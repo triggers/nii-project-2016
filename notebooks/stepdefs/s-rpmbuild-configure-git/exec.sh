d4d211deb9dd13f27ffb899216b1f637db431037
Richard Potter <potter@axsh.net>
2016-04-11 20:14:32 +0900

    Removed lingering reference to aaa()

--- notebooks/stepdefs/s-rpmbuild-configure-git/exec.sh
+++ notebooks/stepdefs/s-rpmbuild-configure-git/exec.sh
@@ -1,4 +1,3 @@
-$(declare -f aaa)
 ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
     $(declare -f check_not_empty)
     check_not_empty ${job} url && echo "Check [ ok ]" || echo "Check [ fail ]"
