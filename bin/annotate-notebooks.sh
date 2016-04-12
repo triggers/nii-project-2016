fb180fdcc56be4858c420ba50d7aa123835dd787
Richard Potter <potter@axsh.net>
2016-04-12 16:38:52 +0900

    Added simple utility for debugging/proofreading to duplicate workbooks with all headings showing

--- bin/annotate-notebooks.sh
+++ bin/annotate-notebooks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If this script is run without any parameters, it will make a copy of
+# each notebook in an ./expanded/ directory, except that all headings
+# will be expanded.  For some types of proof reading this is helpful.
+
+
 export CODEDIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 if [ "$*" = "" ]; then
@@ -13,7 +18,18 @@ fi
 process1()
 {
     while IFS= read -r ln; do
-	printf "%s\n" "$ln"
+	case "$ln" in
+	    *hidden*:*true*)
+		printf "%s\n" "${ln/hidden/hidevalue}"  # hide by renaming to unused value
+		;;
+	    *heading_collapsed*:*true*)
+		printf "%s\n" "${ln/heading_collapsed/hidevalue}" # hide by renaming to unused value
+		:
+		;;
+	    *)
+		printf "%s\n" "$ln"
+		;;
+	esac
     done
 }
 
