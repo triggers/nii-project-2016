fc385e878eee7b27667f1cf9bcdb6a20b1e693dd
toros11 <tobias.rosen@axsh.net>
2016-04-28 22:52:25 +0900

    Fixed problem with path

--- notebooks/stepdefs/s-create-rpm-create-spec-file/exec.sh
+++ notebooks/stepdefs/s-create-rpm-create-spec-file/exec.sh
@@ -1,4 +1,4 @@
-output="$(ssh -qi ../mykeypair root@${INSTANCE_IP} '[[ -f ${HOME}/SPECS/example.spec ]]' 2> /dev/null)"
+output="$(ssh -qi ../mykeypair root@${INSTANCE_IP} '[[ -f ${HOME}/rpmbuild/SPECS/example.spec ]]' 2> /dev/null)"
 
 test_passed=$?
 check_message $test_passed "Created example.spec"
