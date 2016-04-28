9a8570b2c378f65a8a855f53407ab86400f75b54
toros11 <tobias.rosen@axsh.net>
2016-04-28 22:37:10 +0900

    Implemented new check message for jenkins plugin notebooks

--- notebooks/stepdefs/s-jenkins-setup-hipchat/exec.sh
+++ notebooks/stepdefs/s-jenkins-setup-hipchat/exec.sh
@@ -4,24 +4,10 @@ test1_passed=false
 test2_passed=false
 test3_passed=false
 
-check_plugins_exists "hipchat" && test3_passed=true
-check_not_empty system_config "token" <<< "$output" && test1_passed=true
-check_not_empty system_config "room" <<< "$output" && test2_passed=true
+check_plugins_exists "hipchat" && test1_passed=true
+check_not_empty system_config "token" <<< "$output" && test2_passed=true
+check_not_empty system_config "room" <<< "$output" && test3_passed=true
 
-if $test3_passed ; then
-    echo "Plugin Installed: Passed"
-else
-    echo "Plugin Installed: Failed"
-fi
-
-if $test1_passed ; then
-    echo "Token: Passed"
-else
-    echo "Check [ failed ]"
-fi
-
-if $test2_passed ; then
-    echo "Room: Passed"
-else
-    echo "Check [ fail ]"
-fi  
+check_message $test1_passed "$plugin_installed_status"
+check_message $test2_passed "$hipchat_token_status"
+check_message $test3_passed "$hipchat_room_status"
--- notebooks/stepdefs/s-jenkins-setup-rbenv/exec.sh
+++ notebooks/stepdefs/s-jenkins-setup-rbenv/exec.sh
@@ -1,9 +1,4 @@
 test_passed=false
 
 check_plugins_exists "rbenv" && test_passed=true
-
-if $test_passed ; then
-    echo "Plugin Installed: Passed"
-else
-    echo "Plugin Installed: Failed"
-fi
+check_message $test_passed "$plugin_installed_status"
