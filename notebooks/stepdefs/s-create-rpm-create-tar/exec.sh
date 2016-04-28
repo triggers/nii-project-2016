e33650df2840de1a8575e31e2df34a6fe5751554
toros11 <tobias.rosen@axsh.net>
2016-04-28 23:15:40 +0900

    Added scripts for task create rpm create tar

---
+++ notebooks/stepdefs/s-create-rpm-create-tar/exec.sh
@@ -0,0 +1,4 @@
+output="$(ssh -qi /home/centos/mykeypair root@${INSTANCE_IP} '[[ -f ${HOME}/rpmbuild/SOURCES/example-0.1.0.tar.gz ]]' 2> /dev/null)"
+
+test_passed=$?
+check_message $test_passed "Created example-0.1.0.tar.gz"
