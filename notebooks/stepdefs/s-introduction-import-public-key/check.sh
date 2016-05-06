c200e30c4cb0dcafb85d9c8800f3da5ef93a70d2
Richard Potter <potter@axsh.net>
2016-05-06 21:07:49 +0900

    Reduced extra output in s-introduction-import-public-key/check.sh

--- notebooks/stepdefs/s-introduction-import-public-key/check.sh
+++ notebooks/stepdefs/s-introduction-import-public-key/check.sh
@@ -8,11 +8,12 @@ KEYFILE=/tmp/jenkins-ci.org.key
     wget http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key \
 	 -O $KEYFILE 2>/dev/null
 
+KEYID=$(echo $(gpg --throw-keyids < $KEYFILE 2>/tmp/gpg.stderr)|cut -c11-18|tr [A-Z] [a-z])
+
 # from http://unix.stackexchange.com/questions/21226/how-can-i-verify-that-a-pgp-key-is-imported-into-rpm
 
 ssh -qi ../mykeypair root@10.0.2.100 <<EOF 2>/dev/null
-KEYID=$(echo $(gpg --throw-keyids < $KEYFILE)|cut -c11-18|tr [A-Z] [a-z])
-rpm -q gpg-pubkey-\$KEYID
+rpm -q gpg-pubkey-$KEYID
 EOF
 
 # The above used to give excess output, but not I cannot duplicate the problem.
