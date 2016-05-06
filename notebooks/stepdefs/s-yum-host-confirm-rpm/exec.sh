4f7defa6ab43f65b28f01cce1f01b3f99d0b7cd6
Richard Potter <potter@axsh.net>
2016-05-06 19:10:24 +0900

    Reworked s-yum-host-confirm-rpm/exec.sh because assumption about my-script cells was wrong

--- notebooks/stepdefs/s-yum-host-confirm-rpm/exec.sh
+++ notebooks/stepdefs/s-yum-host-confirm-rpm/exec.sh
@@ -2,18 +2,38 @@ output="$(ssh -qi /home/centos/mykeypair root@${INSTANCE_IP} '[[ -f /root/rpmbui
 
 test_passed=$?
 check_message $test_passed "RPM file exists"
-pwd  1>&2
+
 (
     cd .jobs
-    # with -l, grep will output lines like: "j-1042/command"
-    jobs="$(grep -l yum-host-confirm-rpm */command | cut -d / -f 1)"
-    didcheck=false
-    for j in $jobs; do
-	# if input has "ls" and output has the target file name...
-	grep -q "ls" "$j/input" && \
-	    grep -q "example-0.1.0-1.el6.x86_64.rpm" "$j/output" \
-	    && { didcheck=true ; break ; }
-    done
-    check_message $didcheck "Check for RPM file was performed"
+    taskpat='# Task: *'
+
+    # First, filter down the jobs to only look at ones that
+    # have the file name that needs confirming in the output
+    # with -l, grep will output lines like: "j-1042/output"
+    jobs="$(grep -l "example-0.1.0-1.el6.x86_64.rpm" */output | cut -d / -f 1)"
+
+    # Next, check each jobs and eliminate those that are for other
+    # tasks.  Unfortunately, the my-script cells do not give enough
+    # information to tell, so keep all my-script cells.
+    taskjobs="$(
+        for j in $jobs; do
+            input="$(cat "$j/input")"
+            [[ "$input" == $taskpat ]] && \
+                [[ "$input" != *yum-host-confirm-rpm* ]] && continue
+            echo "$j"
+    done)"
+
+    # Finally, check that the input contains "ls"
+    diditjobs="$(
+        for j in $taskjobs; do
+            input="$(cat "$j/input")"
+            [[ "$input" == *ls* ]] && echo "$j"
+    done)"
+
+    [ "$diditjobs" != "" ]
+    check_message $? "Check for RPM file was performed"
 )
 
+# Note: a current limitation of this check is that once a matching job
+# appears in the job list, the second check will always pass, even if
+# the instance is destroyed.
