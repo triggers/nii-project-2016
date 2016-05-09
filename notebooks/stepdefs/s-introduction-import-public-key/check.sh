9204992129564904dc75bd339501f9ce2c737648
Richard Potter <potter@axsh.net>
2016-05-09 16:13:36 +0900

    Updated comments regarding s-introduction-import-public-key/check.sh

--- notebooks/stepdefs/s-introduction-import-public-key/check.sh
+++ notebooks/stepdefs/s-introduction-import-public-key/check.sh
@@ -8,6 +8,9 @@ KEYFILE=/tmp/jenkins-ci.org.key
     wget http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key \
 	 -O $KEYFILE 2>/dev/null
 
+# The next gpg runs in the 1box VM (i.e. not the instance). The first time
+# it is run, it outputs some messages about the setup of ~/.gpg. Now
+# these are sent to a file to keep them from cluttering the Jupyter display.
 KEYID=$(echo $(gpg --throw-keyids < $KEYFILE 2>/tmp/gpg.stderr)|cut -c11-18|tr [A-Z] [a-z])
 
 # from http://unix.stackexchange.com/questions/21226/how-can-i-verify-that-a-pgp-key-is-imported-into-rpm
@@ -15,8 +18,6 @@ KEYID=$(echo $(gpg --throw-keyids < $KEYFILE 2>/tmp/gpg.stderr)|cut -c11-18|tr [
 ssh -qi ../mykeypair root@10.0.2.100 <<EOF 2>/dev/null
 rpm -q gpg-pubkey-$KEYID
 EOF
-
-# The above used to give excess output, but not I cannot duplicate the problem.
 
 [ "$?" = "0" ]
 check_message "$?" "Public key imported"
